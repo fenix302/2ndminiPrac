2136e50c9e89d0eaff2deeae2e510aaee134269f
fenix302 <fenix302@naver.com>
2023-02-28 10:07:48 +0900

    구로 문화센터 백엔드 프로젝트 폴더/ 아이디,비번 guro_cc 계정 생성 필요, hibernate = create 로 실행, 테이블 생성 후 update로 바꿔서 작업 진행

--- src/main/java/com/springboot/user/ServletInitializer.java
+++ src/main/java/com/springboot/user/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(miniPracApplication.class);
+		return application.sources(GuroCCApplication.class);
 	}
 
 }
